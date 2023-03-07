07ba33177e1ed58854bb7c35a15d04c83af363ba
Leyo98765 <124131665+Leyo98765@users.noreply.github.com>
2023-03-07 16:51:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,50 @@
 'use strict';
 
-var libQ = require('kew');
-var fs=require('fs-extra');
-var config = new (require('v-conf'))();
-var exec = require('child_process').exec;
-var execSync = require('child_process').execSync;
+const libQ = require('kew');
+const libFast = require('fast.js');
+const fs = require('fs-extra');
+const path = require('path');
+const exec = require('child_process').exec;
+
+const musicLibrary = '/mnt/USB';
 
 module.exports = oled1;
+
 function oled(context) {
-	var self = this;
-
-	this.context = context;
-	this.commandRouter = this.context.coreCommand;
-	this.logger = this.context.logger;
-	this.configManager = this.context.configManager;
-
+	const self = this;
+	self.context = context;
+	self.commandRouter = self.context.coreCommand;
+	self.logger = self.context.logger;
+	self.configManager = self.context.configManager;
 }
-oled1.prototype.onVolumioStart = function()
-{
-	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
-	this.config = new (require('v-conf'))();
-	this.config.loadFile(configFile);
 
-    return libQ.resolve();
+oled1.prototype.onVolumioStart = function() {
+	const self = this;
+	self.configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
+	self.getConf(self.configFile);
+
+  return libQ.resolve();
 }
 
 oled1.prototype.onStart = function() {
+	const self = this;
+	const defer = libQ.defer();
 
-    var self = this;
-	var defer=libQ.defer();
-	
-	// Once the Plugin has successfull started resolve the promise
-	defer.resolve();
+  // Start the radio
+	self.startRadio();
 
-    return defer.promise;
+  // Set the OLED display to show the name of the currently playing track
+	self.setOledDisplay('Now Playing', 'NRJ');
+
+  // Resolve the promise
+ 	defer.resolve();
+
+  return defer.promise;
 };
 
 oled1.prototype.onStop = function() {
-    var self = this;
-    var defer=libQ.defer();
+    const self = this;
+    const defer=libQ.defer();
 	
 	
 	
@@ -49,3 +54,78 @@ oled1.prototype.onStop = function() {
 
     return libQ.resolve();
 };
+RadioPlugin.prototype.startRadio = function () {
+  const self = this;
+
+  // Get the radio URL from the plugin's configuration file
+  const radioUrl = 'https://www.nrj.fr/webradios';
+
+  // Launch the radio using the mplayer command
+  exec('mplayer -ao alsa:device=hw=1.0 ' + radioUrl, (error, stdout, stderr) => {
+    if (error) {
+      self.logger.error('Failed to start radio: ' + error);
+    }
+  });
+};
+
+RadioPlugin.prototype.setOledDisplay = function (line1, line2) {
+  const self = this;
+
+  // Use the oled1 plugin to set the OLED display text
+  self.commandRouter.executeOnPlugin('system_controller', 'oled1', 'write', [line1, line2]);
+};
+
+RadioPlugin.prototype.getConf = function (configFile) {
+  const self = this;
+
+  self.config = new (require('v-conf'))();
+  self.config.loadFile(configFile);
+};
+
+RadioPlugin.prototype.saveConf = function () {
+  const self = this;
+
+  self.commandRouter.pluginManager.savePluginConfiguration('radio', self.config.get(), function (err, data) {
+    if (err) {
+      self.logger.error('Error saving configuration: ' + err);
+    }
+  });
+};
+
+RadioPlugin.prototype.getUIConfig = function () {
+  const defer = libQ.defer();
+  const self = this;
+
+  const lang_code = self.commandRouter.sharedVars.get('language_code');
+  const configFile = self.commandRouter.pluginManager.getConfigurationFile(self.context, 'config.json');
+
+  const config = {
+    page: {
+      title: 'Radio Plugin Settings',
+      plugin: 'radio'
+    },
+    sections: []
+  };
+
+  defer.resolve(config);
+
+  return defer.promise;
+};
+
+RadioPlugin.prototype.setUIConfig = function (data) {
+  const self = this;
+
+  self.logger.info('Received UIConfig:', data);
+};
+
+RadioPlugin.prototype.getConfigurationFiles = function () {
+  return ['config.json'];
+};
+
+RadioPlugin.prototype.addToBrowseSources = function () {
+  const self = this;
+  return self.commandRouter.v
+	
+}
+
+
