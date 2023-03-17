84b68d8d76078f8d60d939387afaa72fbbd9d43b
camilleat <118841268+camilleat@users.noreply.github.com>
2023-03-17 14:48:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-'use strict'; //Assure le strict mode du plugin pour éviter les erreurs de code
+'use strict';
 
 var libQ = require('kew');
-var fs=require('fs-extra');
-const path=require('path');
 var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
-
-const Gpio = require('onoff').Gpio;
-const io = require('socket.io-client');
+var config = new(require('v-conf'))();
+var RadioStation = "http://your_radio_station_url_here.com/stream" //changer l'url de la radio
 
 //Constructeur ----------------------------------------------------------------------------------------------------------------------------------------------------
 module.exports = oled1;
@@ -24,91 +20,33 @@ function Oled1(context) {
 oled1.prototype.onVolumioStart = function()
 {
 	var self = this;
-	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json');
+	var configFile=this.commandRouter.pluginManager.getConfigurationFile(this.context,'config.json'); //self ou this ???
 	this.config = new (require('v-conf'))();
 	this.config.loadFile(configFile);
 
     return libQ.resolve();
 }
 
-
 //Configure les settings quand le plugin PapyRadio démarre ------------------------------------------------------------------------------------------------------------
 oled1.prototype.onStart = function() {
     	var self = this;
-	var defer=libQ.defer();
-	
-	self.debugLogging = (self.config.get('logging')==true);
-	self.status=null;
-	self.loadI18nStrings();
+	self.logger.info("Starting oled1");
+	return libQ.resolve();
 
-	if (self.debugLogging) self.logger.info('[OLED1] onStart: Config loaded: ' + JSON.stringify(self.config));
-
-	//self.socket = io.connect('http://localhost:3000');
-	//self.socket.emit('getState');
-	//self.socket.on('pushState',function(data){
-		//self.status = data;
-	//	self.lastTime = data.seek - Date.now();
-		// if (self.debugLogging) self.logger.info('[OLED1] received Websock Status: ' + JSON.stringify(self.status));
-	//})
-
-	//.then(_=> {
-		//self.commandRouter.pushToastMessage('success',"Oled1 - successfully loaded")
-		//if (self.debugLogging) self.logger.info('[OLED1] onStart: Plugin successfully started.');				
-		//defer.resolve();				
-	//})
-	//.fail(error => {
-	//	self.commandRouter.pushToastMessage('error',"oled", self.getI18nString('OLED1.TOAST_STOP_FAIL'))
-	//	defer.reject();
-	//});
-	//let station = 'https://www.radio-en-ligne.fr/nrj';
-	//self.playRadio(station);
-
-    return defer.promise;
 };
-
-
 
 //Configure les settings quand le plugin oled1 s'arrête ------------------------------------------------------------------------------------------------------------
 oled1.prototype.onStop = function() {
-    var self = this;
-    var defer=libQ.defer();
-
-	if (self.debugLogging) self.logger.info('[OLED1] onStop: Stopping Plugin.');
-
-	.then(_=> {
-		self.socket.off('pushState');
-		self.socket.disconnect();
-	})
-	.then(_=>{
-		self.commandRouter.pushToastMessage('success',"oled1", self.getI18nString('OLED1.TOAST_STOP_SUCCESS'))
-		if (self.debugLogging) self.logger.info('[OLED1] onStop: Plugin successfully stopped.');				
-		defer.resolve();	
-	})
-	.fail(err=>{
-		self.commandRouter.pushToastMessage('success',"Oled1", self.getI18nString('OLED1.TOAST_STOP_FAIL'))
-		self.logger.error('[OLED1] onStop: Failed to cleanly stop plugin.'+err);				
-		defer.reject();	
-	})
-    return defer.promise;
+	var self = this;
+	self.logger.info("Stopping oled1");
+ 	return libQ.resolve(); //si ca marche pas mettre : var defer=libQ.defer();
 };
-
 
 //Configure les settings quand le plugin PapyRadio redémarre ------------------------------------------------------------------------------------------------------------
 oled1.prototype.onRestart = function() {
-    var self = this;
-    var defer=libQ.defer();
-
-	if (self.debugLogging) self.logger.info('[OLED1] onRestart: free resources');
+    	var self = this;
+	return libQ.resolve();
 };
-
-//oled1.prototype.playRadio = function(station){
- //  socket.emit('replaceAndPlay', {
-    //    service:'webradio',
-     //   type:'webradio',
-      //  title:station,
-      // uri: station
-  //  });
-//}
 
 
 //Configuration Methods ------------------------------------------------------------------------------------------------------------------------------------------
